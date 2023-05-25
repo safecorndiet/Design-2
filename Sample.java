a19d917b26680a339749c4ba8aca5c81d1e2c381
Divy Shah <divyshah@trentu.ca>
2023-05-24 21:36:05 -0400

    Completed Design-2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,45 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : o(1)
+// Space Complexity : o(n)
+// Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+class MyHashSet {
+    private int numBuckets;
+    private LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        numBuckets = 1000;
+        buckets = new LinkedList[numBuckets];
+    }
+
+    private int hash(int key) {
+        return key % numBuckets;
+    }
+
+    public void add(int key) {
+        int bucket = hash(key);
+        if (buckets[bucket] == null) {
+            buckets[bucket] = new LinkedList<>();
+        }
+        if (!buckets[bucket].contains(key)) {
+            buckets[bucket].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int bucket = hash(key);
+        if (buckets[bucket] != null) {
+            buckets[bucket].remove(Integer.valueOf(key));
+        }
+    }
+
+    public boolean contains(int key) {
+        int bucket = hash(key);
+        if (buckets[bucket] != null) {
+            return buckets[bucket].contains(key);
+        }
+        return false;
+    }
+}
